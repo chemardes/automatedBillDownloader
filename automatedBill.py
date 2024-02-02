421c414c6643394711c76f83af1b3da67972fd7a
Chelsea <chedemars08@gmail.com>
2024-02-02 21:53:49 +0000

    FEATURE: initial commit

--- automatedBill.py
+++ automatedBill.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
+# setting chrome options
 options = Options()
 # leaves browser open even after completion
 options.add_experimental_option("detach", True)
@@ -22,10 +21,15 @@ driver.implicitly_wait(5)
 driver.find_element("xpath", "//button[@id='onetrust-accept-btn-handler']").click()
 
 # automatic login
-driver.find_element("xpath", "//input[@id='login-username']").send_keys("marco04divaio@yahoo.com")
-driver.find_element("xpath", "//input[@id='login-password']").send_keys("141577mcM%@%@@")
+driver.find_element("xpath", "//input[@id='login-username']").send_keys("*************")
+driver.find_element("xpath", "//input[@id='login-password']").send_keys("*************")
 driver.find_element("xpath", "//button[@id='submitButton']").click()
 
 # downloading electricity bill
-driver.find_element("xpath", "//button[@id='last-bill-btn-link-2']").click();
+driver.find_element("xpath", "//button[@id='last-bill-btn-link-2']").click()
+
+# redirects user to next tab (contains the pdf file)
+driver.switch_to.window(driver.window_handles[1])
+driver.find_element("xpath", "//cr-icon-button[@id='download']").click()
+
 
