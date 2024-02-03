972a8be42f88a2fb88cf720c1e79083f7debb567
Chelsea <chedemars08@gmail.com>
2024-02-03 14:45:58 +0000

    FEATURE: fixing code to download pdf externally + access to pdf file

---
+++ README.md
@@ -0,0 +1,9 @@
+## Automatic Bill Downloader
+
+<p>Before running this downloader, please ensure that you have the necessary packages installed.</p>
+
+
+```
+pip install selenium
+pip install webdriver-manager
+```
--- automatedBill.py
+++ automatedBill.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+
+import readPDF
+
+file_path = r"C:\Users\LEGION\Downloads"
 
 # setting chrome options
 options = Options()
 # leaves browser open even after completion
 options.add_experimental_option("detach", True)
+options.add_experimental_option('prefs', {
+    "plugins.always_open_pdf_externally": True,
+    "download_default_directory": file_path}) # change file path accordingly
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                           options=options)
@@ -14,22 +23,29 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
 driver.get("https://energyonline.energia.ie/")
 driver.maximize_window()
 
+# handling cookie popup
+cookie_popup = driver.find_element("xpath", "//button[@id='onetrust-accept-btn-handler']")
 # waits for page to load
-driver.implicitly_wait(5)
-
-# handling cookie popups
-driver.find_element("xpath", "//button[@id='onetrust-accept-btn-handler']").click()
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable(cookie_popup))
+cookie_popup.click()
 
 # automatic login
-driver.find_element("xpath", "//input[@id='login-username']").send_keys("*************")
-driver.find_element("xpath", "//input[@id='login-password']").send_keys("*************")
-driver.find_element("xpath", "//button[@id='submitButton']").click()
+login_username = driver.find_element("xpath", "//input[@id='login-username']")
+login_password = driver.find_element("xpath", "//input[@id='login-password']")
+login = driver.find_element("xpath", "//button[@id='submitButton']")
+
+login_username.send_keys("**********")
+login_password.send_keys("**********")
+login.click()
 
 # downloading electricity bill
-driver.find_element("xpath", "//button[@id='last-bill-btn-link-2']").click()
+driver.implicitly_wait(5)
+bill = driver.find_element("xpath", "//button[@id='last-bill-btn-link-2']")
+bill.click()
 
 # redirects user to next tab (contains the pdf file)
+WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
 driver.switch_to.window(driver.window_handles[1])
-driver.find_element("xpath", "//cr-icon-button[@id='download']").click()
 
+latest_download = readPDF.getting_latest_downloaded_folder(file_path)
 
